b627a7a88e88f07642da6d2970499dfe21b3414f
Comeoksal <keumsiun0503@gmail.com>
2025-12-01 03:33:01 +0900

    :sparkles: Feat: img_sticker

--- view/img_sticker_setting_view.py
+++ view/img_sticker_setting_view.py
@@ -3,30 +3,28 @@ from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import Qt
 from model.settings import Settings
 
-class EmojiStickerSettingView(QWidget):
+class ImgStickerSettingView(QWidget):
     def __init__(self, main_processor):
         super().__init__()
-        self.setWindowTitle('이모지 세팅 및 문구 추가')
+        self.setWindowTitle('추가 이미지 세팅')
         self.setWindowIcon(QIcon(Settings.MAIN_ICON))
         self.setFixedSize(2000, 1500)
 
         self.top_bar = QHBoxLayout()
-        self.save_btn = QPushButton("⭐이미지 저장⭐")
-        self.input = QLineEdit(self)
-        self.text_btn = QPushButton("입력")
+        self.next_btn = QPushButton("다음으로")
+        self.crop_btn = QPushButton("영역 자르기")
         self.set_top_bar()
 
         self.left_bar = QVBoxLayout()
-        self.emoji_btn = []
+        self.img_sticker_btn = QPushButton()
+        self.img_load_btn = QPushButton("이미지\n불러오기")
         self.undo_btn = QPushButton("실행\n취소")
         self.redo_btn = QPushButton("다시\n실행")
         self.set_left_bar()
 
         self.main_area = QLabel()
-        self.main_area.setStyleSheet("background-color: #d9d9d9; border: 1px solid black;")
-        self.main_area.setAlignment(Qt.AlignCenter)
         pix = Settings.convert_to_pyqt_img(main_processor.four_cut)
-        self.main_area.setPixmap(pix)
+        self.set_middle(pix)
 
         self.layout = QVBoxLayout()
 
@@ -39,6 +37,7 @@ class EmojiStickerSettingView(QWidget):
         # (좌측 바 + 메인 영역) 위젯 추가
         self.left_widget = QWidget()
         self.left_widget.setLayout(self.left_bar)
+        self.left_widget.setFixedWidth(400)
         self.left_widget.setStyleSheet("background-color: #d9d9d9; border: 1px solid black;")
 
         self.middle = QHBoxLayout()
@@ -56,52 +55,35 @@ class EmojiStickerSettingView(QWidget):
         self.main_area.setPixmap(pix)
 
     def set_top_bar(self):
-        self.save_btn.setFixedSize(200, 100)
-        self.input.setFixedSize(500, 50)
-        self.input.setStyleSheet("background-color: white;")
-        self.text_btn.setFixedSize(100, 50)
-        self.text_btn.setStyleSheet("background-color: white;")
+        self.next_btn.setFixedSize(200, 100)
         center_layout = QHBoxLayout()
-        center_layout.addWidget(self.input)
-        center_layout.addWidget(self.text_btn)
         center_layout.addSpacing(20)
 
-        self.top_bar.addWidget(self.save_btn)
+        self.crop_btn.setFixedSize(150, 80)
+        self.crop_btn.setStyleSheet("background-color: #FFFFFF; color: black; font-weight: bold;")
+
+        self.top_bar.addWidget(self.next_btn)
         self.top_bar.addStretch()
-        self.top_bar.addLayout(center_layout)
+        self.top_bar.addWidget(self.crop_btn)
         self.top_bar.addStretch()
         
     def set_left_bar(self):
-
         self.left_bar.setContentsMargins(20, 20, 20, 20)
-        emojies = Settings.EMOJIES
+        self.left_bar.addStretch()
 
-        for i in range(0, len(emojies), 2):
-            row = QHBoxLayout()
+        # img_sticker에 대한 row
+        row = QHBoxLayout()
+        self.img_sticker_btn.setFixedSize(300, 400)
+        self.img_sticker_btn.setStyleSheet("background-color: #FFFFFF;")
+        row.addWidget(self.img_sticker_btn)
+        self.left_bar.addLayout(row)
 
-            btn1 = QPushButton()
-            btn1.setFixedSize(80, 80)
-            btn1.setProperty("emoji", i)
-            pix1 = Settings.convert_to_pyqt_img(emojies[i])
-            btn1.setIcon(QIcon(pix1))
-            btn1.setIconSize(btn1.size())
-            btn1.setStyleSheet("border-radius: 25px; border: 1px solid #ccc;")
-            self.emoji_btn.append(btn1)
-            row.addWidget(btn1)
-
-            if i + 1 < len(emojies):
-                btn2 = QPushButton()
-                btn2.setFixedSize(80, 80)
-                btn2.setProperty("emoji", i+1)
-                pix2 = Settings.convert_to_pyqt_img(emojies[i+1])
-                btn2.setIcon(QIcon(pix2))
-                btn2.setIconSize(btn2.size())
-                btn2.setStyleSheet("border-radius: 25px; border: 1px solid #ccc;")
-                self.emoji_btn.append(btn2)
-                row.addWidget(btn2)
-
-            self.left_bar.addLayout(row)
-        
+        # img_load에 대한 row
+        row = QHBoxLayout()
+        self.img_load_btn.setFixedSize(160, 100)
+        self.img_load_btn.setStyleSheet("background-color: #FFFFFF; color: black; font-weight: bold;")
+        row.addWidget(self.img_load_btn)
+        self.left_bar.addLayout(row)
         self.left_bar.addStretch()
 
         # undo, redo에 대한 row
@@ -114,6 +96,16 @@ class EmojiStickerSettingView(QWidget):
         row.addWidget(self.undo_btn)
         row.addWidget( self.redo_btn)
         self.left_bar.addLayout(row)
+
+    def set_middle(self, pix):
+        self.main_area.setStyleSheet("background-color: #d9d9d9; border: 1px solid black;")
+        self.main_area.setAlignment(Qt.AlignCenter)
+        self.main_area.setPixmap(pix)
+
+    def set_img_sticker_btn(self, loaded_img):
+        pix = Settings.convert_to_pyqt_img(loaded_img)
+        self.img_sticker_btn.setIcon(QIcon(pix))
+        self.img_sticker_btn.setIconSize(pix.size())
 
     def center(self):
         qr = self.frameGeometry()
--- view/take_picture_view.py
+++ view/take_picture_view.py
@@ -1,9 +1,12 @@
-from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel, QDesktopWidget
-from PyQt5.QtCore import QTimer, Qt
+from PyQt5.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel, QDesktopWidget, QHBoxLayout
+from PyQt5.QtCore import QTimer, Qt, pyqtSignal, QObject
 from PyQt5.QtGui import QIcon
 from model.settings import Settings
 import cv2
 import numpy as np
+
+class Communicate(QObject):
+    time_out = pyqtSignal()
 
 class TakePictureView(QWidget):
     def __init__(self):
@@ -12,12 +15,26 @@ class TakePictureView(QWidget):
         self.setWindowIcon(QIcon(Settings.MAIN_ICON))
         self.setFixedSize(2000, 1500)
 
+        self.current_sec = Settings.CAMERA_TIME
+        self.c = Communicate()
+
+        self.top_bar = QHBoxLayout()
+        self.second_count = QLabel(self)
+        self.second_count.setText(f'{self.current_sec}')
+        self.second_count.setFixedSize(200, 200)
+        self.second_count.setStyleSheet("background-color: rgb(255, 255, 255); color: black; font-size: 60px; font-weight: bold;")
+        self.second_count.setAlignment(Qt.AlignCenter)
+
         self.capture_count = QLabel(self)
         self.capture_count.setGeometry(800, 30, 400, 150)
-        self.capture_count.setStyleSheet("background-color: rgba(0, 0, 0, 100); color: white; font-size: 40px; font-weight: bold;padding: 10px;")
+        self.capture_count.setStyleSheet("background-color: rgba(0, 0, 0, 100); color: white; font-size: 40px; font-weight: bold; padding: 10px;")
         self.capture_count.setAlignment(Qt.AlignCenter)
         self.capture_count.setText("0 / 4")
         self.capture_count.raise_()
+        self.top_bar.addWidget(self.second_count)
+        self.top_bar.addStretch()
+        self.top_bar.addWidget(self.capture_count)
+        self.top_bar.addStretch()
 
         self.cam = QLabel(self)
         self.cam.setAlignment(Qt.AlignCenter)
@@ -42,6 +59,10 @@ class TakePictureView(QWidget):
         self.timer.timeout.connect(self.update_frame)
         self.timer.start(30)
 
+        self.sec_timer = QTimer()
+        self.sec_timer.timeout.connect(self.update_count)
+        self.sec_timer.start(1000)
+
     def update_frame(self):
         if self.isCapturing:
             black = np.zeros((Settings.TARGET_HEIGHT*3, Settings.TARGET_WIDTH*4, 3), np.uint8)
@@ -57,6 +78,16 @@ class TakePictureView(QWidget):
         enhanced_frame = Settings.enhance_frame(frame)
         pix = Settings.convert_to_pyqt_img(cv2.flip(enhanced_frame, 1))
         self.cam.setPixmap(pix)
+
+    def update_count(self):
+        self.current_sec -= 1
+        self.second_count.setText(f'{self.current_sec}')
+        if self.current_sec <= 3:
+            self.second_count.setStyleSheet("background-color: rgb(255, 255, 255); color: red; font-size: 60px; font-weight: bold;")
+        if self.current_sec == 0:
+            self.sec_timer.stop()
+            self.c.time_out.emit()
+            self.sec_timer.start(1000)
 
     def center(self):
         qr = self.frameGeometry()
